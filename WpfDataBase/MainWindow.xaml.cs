e1b6959ef80c08277a9c3ed8ca106f475c436148
Norbert Barna <norbert.barna@semilab.hu>
2019-12-02 19:42:33 +0100

    modifications

---
+++ WpfDataBase/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace WpfDataBase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string _connectionString;
+        private static SqlConnection _connection;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _connectionString = ConfigurationManager.ConnectionStrings["WpfDataBase.Properties.Settings.Test01_ConnectionString"].ConnectionString;
+
+            PopulatePersons();
+
+        }
+
+
+
+        private void PopulatePersons()
+        {
+            string sqlQuery = "SELECT p.Firstname, p.Lastname, p.Age, Workplace.WorkplaceName" +
+                                "FROM Person AS p" +
+                                "INNER JOIN Workplace ON Workplace.Id = p.Workplace";
+
+            using (_connection = new SqlConnection(_connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(sqlQuery, _connection))
+            {
+                {
+                    DataTable personTable = new DataTable();
+                    DataSet personDataSet = new DataSet();
+
+                    adapter.Fill(personTable);
+                    adapter.Fill(personDataSet, "Person");
+
+                    datagrid.DataContext = personTable.DefaultView;
+                    //listbox.ItemsSource = personTable.AsDataView();
+                }
+
+
+            }
+
+        }
+
+
+    }
+}
