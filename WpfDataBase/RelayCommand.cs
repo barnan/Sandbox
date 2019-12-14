7fc409cce82aa375e41c62e50f7f6999d9102982
bankabt <barnorcsa@gmail.com>
2019-12-14 22:04:18 +0100

    impl

---
+++ WpfDataBase/RelayCommand.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfDataBase
+{
+    internal class RelayCommand : NotifyBase, ICommand
+    {
+        #region fields
+
+        private readonly Action<object> _parameterizedAction;
+        private readonly Action _simpleAction;
+        private readonly Predicate<object> _canExecute;
+
+        #endregion
+
+        #region ctor
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _parameterizedAction = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Predicate<object> canExecute)
+        {
+            _simpleAction = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion
+
+        #region properties
+
+        private bool _isEnabled = true;
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set
+            {
+                _isEnabled = value;
+                OnPropertyChanged();
+
+                var eventHandlerToCall = CanExecuteChanged;
+                eventHandlerToCall?.Invoke(this, new EventArgs());
+            }
+        }
+
+        public void UpdateCanExecute()
+        {
+            EventHandler handler = CanExecuteChanged;
+            handler?.Invoke(this, new EventArgs());
+        }
+
+        #endregion
+
+        #region ICommand
+
+        public event EventHandler CanExecuteChanged;
+        //{
+        //    add { CommandManager.RequerySuggested += value; }       // todo: look after -> what is CommandManager
+        //    remove { CommandManager.RequerySuggested -= value; }
+        //}
+
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled && (_canExecute == null || _canExecute(parameter));
+        }
+
+        public void Execute(object parameter)
+        {
+            _simpleAction?.Invoke();
+            _parameterizedAction?.Invoke(parameter);
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ WpfDataBase/SQL_Queries/dbo.VerbView.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW VerbView(Id, Present, PresentE3, SimplePast, Aux, Perfect) AS 
+SELECT v.Id, 
+	W1.Word AS Present, 
+	W2.Word AS PresentE3, 
+	W3.Word AS SimplePast, 
+	Aux.AuxiliaryVerb As Aux,
+	W4.Word AS Perfect
+FROM VerbTable AS v
+INNER JOIN WordTable AS W1 ON W1.Id=v.Present
+INNER JOIN WordTable AS W2 ON W2.Id=v.PresentE3
+INNER JOIN WordTable AS W3 ON W3.Id=v.SimplePast
+INNER JOIN AuxiliaryVerbTable AS Aux ON Aux.Id=v.AuxiliaryVerb
+INNER JOIN WordTable AS W4 ON W4.Id=v.Perfect
