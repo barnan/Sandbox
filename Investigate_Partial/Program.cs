94c50d8c00ca31250b3d1bc0c23fdd990edfa068
Norbert Barna <norbert.barna@semilab.hu>
2020-02-10 19:25:57 +0100

    added investigate partial

---
+++ Investigate_Partial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Investigate_Partial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Valami valami = new Valami();
+            valami.DoSomething2();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Investigate_Partial/Valami.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Investigate_Partial
+{
+
+    internal class BaseClass1
+    {
+        private int Proper { get; set; }
+    }
+
+
+    internal class BaseClass2
+    {
+        private string Prop { get; set; }
+    }
+
+
+
+    internal partial class Valami : BaseClass1          // több helyen is meg lehet adni baseclass-t de annak ugyannak kell lennie, így sem lehet több őse, mint 1
+    {
+
+        //partial void DoSomething()
+        //{
+        //    Console.WriteLine("valami DoSomething 1");          // egyszerre csak egy implementáció lehet
+        //}
+    }
+
+
+
+    internal partial class Valami : BaseClass1
+    {
+        partial void DoSomething();
+
+
+        public void DoSomething2()
+        {
+            DoSomething();
+        }
+
+    }
+
+}
