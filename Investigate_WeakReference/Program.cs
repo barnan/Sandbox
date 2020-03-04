c34a26931982e4506093f18572e1141034d84907
Norbert Barna <norbert.barna@semilab.hu>
2020-03-04 15:42:06 +0100

    Investigate weakreference added

---
+++ Investigate_WeakReference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Investigate_WeakReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WeakReference wr = new WeakReference(new StringBuilder());
+
+            var targetData = wr.Target as StringBuilder;
+            targetData?.Append("Ha Ha, He He, Hi Hi");
+
+            Console.WriteLine($"1. isalive: {wr.IsAlive}");         // debugban máshogy megy a GC optimalizálása. Debugban -> True, True, Releaseben -> True, False
+            Console.WriteLine(targetData.ToString());
+
+            GC.Collect();
+
+            Console.WriteLine($"2. isalive: {wr.IsAlive}");
+
+            Console.ReadKey();
+        }
+    }
+}
