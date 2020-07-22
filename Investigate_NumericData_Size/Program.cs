c1c928d5be78d94061ef52d70cf5bd71d9fe3b41
Norbert Barna <norbert.barna@semilab.hu>
2020-07-22 18:09:34 +0200

    added investigate numeric data size

---
+++ Investigate_NumericData_Size/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Investigate_NumericData_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"byte {sizeof(byte)}");
+            Console.WriteLine($"sbyte {sizeof(sbyte)}");
+            Console.WriteLine($"char {sizeof(char)}");
+            Console.WriteLine($"short {sizeof(short)}");
+            Console.WriteLine($"ushort {sizeof(ushort)}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
