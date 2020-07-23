5532d138c4d0288ca9b7439a9961634b501cc5c6
Norbert Barna <norbert.barna@semilab.hu>
2020-07-23 09:14:48 +0200

    modifications

--- Investigate_NumericData_Size/Program.cs
+++ Investigate_NumericData_Size/Program.cs
@@ -6,12 +6,21 @@ namespace Investigate_NumericData_Size
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"bool {sizeof(bool)}");
             Console.WriteLine($"byte {sizeof(byte)}");
             Console.WriteLine($"sbyte {sizeof(sbyte)}");
             Console.WriteLine($"char {sizeof(char)}");
             Console.WriteLine($"short {sizeof(short)}");
             Console.WriteLine($"ushort {sizeof(ushort)}");
-
+            Console.WriteLine($"Int16 {sizeof(Int16)}");
+            Console.WriteLine($"Int32 {sizeof(Int32)}");
+            Console.WriteLine($"int {sizeof(int)}");
+            Console.WriteLine($"Int64 {sizeof(Int64)}");
+            Console.WriteLine($"UInt64 {sizeof(UInt64)}");
+            Console.WriteLine($"long {sizeof(long)}");
+            Console.WriteLine($"float {sizeof(float)}");
+            Console.WriteLine($"double {sizeof(double)}");
+            Console.WriteLine($"decimal {sizeof(decimal)}");
 
             Console.ReadKey();
         }
