43ef5ca230a4e9214199d2b09a57fabcf73700f1
Norbert Barna <norbert.barna@semilab.hu>
2020-09-02 12:42:29 +0200

    mod

---
+++ Examine_Warnings_Pragma/Program.cs
@@ -0,0 +1,32 @@
+﻿#define MANCIKA
+
+
+namespace Examine_Warnings_Pragma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+#if (MANCIKA && NET40)
+
+
+#pragma warning disable 1030
+
+#warning 6000 MANCIKA is defined
+
+#pragma warning restore
+
+#warning 6000 MANCIKA is defined
+
+#endif
+
+
+            //#error this code sucks
+
+
+        }
+    }
+}
