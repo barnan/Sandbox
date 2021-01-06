8e42d032bc1dd72a7db2807761a468e7b2f18095
Norbert Barna <norbert.barna@semilab.hu>
2021-01-06 11:40:39 +0100

    added retry to unittestproject

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -7,8 +7,10 @@ namespace UnitTestProject
     [TestFixture]
     public class UnitTest1
     {
+        private int counter = 0;
 
         [Test]
+        [Ignore("csak")]
         public void TestMethod1()
         {
             var mock = new Mock<IFoo>();
@@ -28,13 +30,25 @@ namespace UnitTestProject
             Assert.That(true);
         }
 
-
         public void DoSeomthing(IFoo ifoo)
         {
             //bool result = ifoo.DoSomething("w");
             ifoo.Name = "pistika";
         }
 
-    }
+        [Test]
+        [Retry(3)]
+        [TestCase(10, 10)]
+        [TestCase(20, 20)]
+        [TestCase(30, 50)]
+        [TestCase(40, 40)]
+        [TestCase(50, 50)]
+        [TestCase(10, 20)]
+        public void TestMethod2(int hehe, int haha)
+        {
+            counter++;
 
+            Assert.That(hehe, Is.EqualTo(haha));
+        }
+    }
 }
