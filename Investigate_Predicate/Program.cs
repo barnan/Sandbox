167e28721f5c3f6ab758c04a511941cab35f821a
Norbert Barna <norbert.barna@semilab.hu>
2021-01-15 10:34:50 +0100

    added predicate investigation

---
+++ Investigate_Predicate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Investigate_Predicate
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Action action01 = new Action(Method01);
+
+            Func<bool> func01 = new Func<bool>(Method02);
+            Func<bool, string, bool> func02 = new Func<bool, string, bool>(Method03);
+
+            Predicate<string> predicate01 = new Predicate<string>(Method04);
+
+            action01();
+            Console.WriteLine(func01());
+            Console.WriteLine(func02(false, "hehe"));
+            Console.WriteLine(predicate01("hehe"));
+
+            Console.ReadKey();
+        }
+
+        private static void Method01()
+        {
+            Console.WriteLine(nameof(Method01));
+        }
+
+        private static bool Method02()
+        {
+            Console.WriteLine(nameof(Method02));
+            return true;
+        }
+
+        private static bool Method03(bool input1, string input2)
+        {
+            Console.WriteLine(nameof(Method03));
+            return !input1 && string.IsNullOrEmpty(input2);
+        }
+
+        private static bool Method04(string input)
+        {
+            Console.WriteLine(nameof(Method04));
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
--- Regex/Program.cs
+++ Regex/Program.cs
@@ -1,39 +1,33 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-
 [assembly: CLSCompliant(true)]
-
-
 
 namespace RegexPelda
 {
-    class Program
+    internal class Program
     {
         public static UInt32 Valami { get; set; }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-        //    string text = "valami 30000";
-        //    string text1 = "valami 300.01";
-        //    string text2 = "valami 300";
+            //    string text = "valami 30000";
+            //    string text1 = "valami 300.01";
+            //    string text2 = "valami 300";
 
+            //    Match mat = Regex.Match(text, @"\d+");
+            //    string[] content = Regex.Split(text, "[^\\d]");
+            //    string resu = string.Join(null, content);
 
-        //    Match mat = Regex.Match(text, @"\d+");
-        //    string[] content = Regex.Split(text, "[^\\d]");
-        //    string resu = string.Join(null, content);
+            //    Match mat2 = Regex.Match(text1, @"[0-9]*\.?[0-9]+");
+            //    string[] contentfloat = Regex.Split(text1, "[^\\d.]");
+            //    string resufloat = string.Join(null, contentfloat);
 
+            //    Match mat3 = Regex.Match(text2, @"[0-9]*\.?[0-9]+");
+            //    string[] contentfloat2 = Regex.Split(text2, "[^\\d.]");
+            //    string resufloat2 = string.Join(null, contentfloat2);
 
-        //    Match mat2 = Regex.Match(text1, @"[0-9]*\.?[0-9]+");
-        //    string[] contentfloat = Regex.Split(text1, "[^\\d.]");
-        //    string resufloat = string.Join(null, contentfloat);
-
-
-        //    Match mat3 = Regex.Match(text2, @"[0-9]*\.?[0-9]+");
-        //    string[] contentfloat2 = Regex.Split(text2, "[^\\d.]");
-        //    string resufloat2 = string.Join(null, contentfloat2);
-
-
+            #region 1
 
             string pattern = @"\[A, '[^\[\]]*'\]";
             Regex rgx = new Regex(pattern);
@@ -41,6 +35,31 @@ namespace RegexPelda
 
             foreach (Match match in rgx.Matches(sentence))
                 Console.WriteLine("Found '{0}' at position {1}", match.Value, match.Index);
+
+            #endregion 1
+
+            Console.WriteLine(Environment.NewLine);
+
+            #region 2
+
+            string pattern2 = @"\[U4, [\d^\[\]]+L\]";
+            string pattern3 = @" [\d]+";
+            //string pattern3 =
+            Regex rgx2 = new Regex(pattern2);
+            Regex rgx3 = new Regex(pattern3);
+            string sentence2 = "Device Message To Host S6F11 W1 Body [L, [U4, 106L], [U4, 1001L], [L, [L, [U4, 1179L], [L, [A, 'Carrier1'], [A, ''], [A, '24'], [A, 'Carrier1_24'], [A, '13/01/2021 09:33:45'], [A, '00:00:07.3207604'], [A, 'Completed'], [F8, 1.0], [F8, 1.0], [F8, 1.0], [F8, 0.0], [F8, 37.09220240815438], [F8, 37.09220240815438], [F8, 37.09220240815438], [F8, 0.0], [F8, NaN], [F8, NaN], [F8], [F8], [F8], [F8]]]]]";
+
+            foreach (Match match2 in rgx2.Matches(sentence2))
+            {
+                Console.WriteLine("Found '{0}' at position {1}", match2.Value, match2.Index);
+
+                foreach (Match match3 in rgx3.Matches(match2.ToString()))
+                {
+                    Console.WriteLine("Found '{0}' at position {1}", match3.Value, match3.Index);
+                }
+            }
+
+            #endregion 2
 
             Console.ReadKey();
         }
